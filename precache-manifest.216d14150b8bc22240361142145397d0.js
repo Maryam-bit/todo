b8614f1f12e7b99fd7a918b9c94854f45999e40b
Maryam Noor <rajputmaryam5555@gmail.com>
2020-10-09 23:56:08 +0500

    Updates

---
+++ precache-manifest.216d14150b8bc22240361142145397d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350d24b91a0ba301fbfbda237fde35f0",
+    "url": "/Maryam-bit/todo.git/index.html"
+  },
+  {
+    "revision": "d9118c36534c3c04a0be",
+    "url": "/Maryam-bit/todo.git/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e0ac86224a0c78bdd7e2",
+    "url": "/Maryam-bit/todo.git/static/js/2.3494eab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Maryam-bit/todo.git/static/js/2.3494eab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9118c36534c3c04a0be",
+    "url": "/Maryam-bit/todo.git/static/js/main.f43a7205.chunk.js"
+  },
+  {
+    "revision": "393844a35acf6a2e6e09",
+    "url": "/Maryam-bit/todo.git/static/js/runtime-main.0e8010b7.js"
+  }
+]);
